58c847c807ab707e4106b254b2e2c90fb771ef0d
Anuprienko Stanislav <anuper@yandex.ru>
2022-07-13 15:43:03 +0300

    refactor: fix bugs

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ console.log("Score: 100");
 const menu = document.querySelector('.header__items');
 const burger = document.querySelector('.burger');
 const burgerSpan = document.querySelector('.burger__span');
+const shadow = document.querySelector('.shadow');
 const body = document.body;
 const link = document.querySelector('.header__link');
 const links = Array.from(menu.children);
@@ -18,6 +19,7 @@ function openBurger() {
     burger.classList.toggle('burger_active');
     burgerSpan.classList.toggle('burger__span_active');
     body.classList.toggle('body_noscroll');
+    shadow.classList.toggle('shadow_active');
 }
 
 function closeBurger() {
@@ -25,4 +27,5 @@ function closeBurger() {
     burger.classList.remove('burger_active');
     burgerSpan.classList.remove('burger__span_active');
     body.classList.remove('body_noscroll');
+    shadow.classList.remove('shadow_active');
 }
